7fd76dd22f1f5eb2deb1a707abe7990742ff8183
ruoyuangao <89848323+ruoyuangao@users.noreply.github.com>
2022-07-29 11:35:19 -0500

    use lua for invertory functions, prevent over sell

--- seckill/src/main/java/com/jiuzhang/seckill/services/RedisService.java
+++ seckill/src/main/java/com/jiuzhang/seckill/services/RedisService.java
@@ -5,6 +5,7 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 
 @Service
 public class RedisService {
@@ -26,4 +27,35 @@ public class RedisService {
         client.close();
         return value;
     }
+
+    /** * 缓存中库存判断和扣减 * @param key * @return * @throws Exception */
+    public boolean stockDeductValidation(String key) {
+        try(Jedis client = jedisPool.getResource()) {
+            String script = "if redis.call('exists',KEYS[1]) == 1 then\n" +
+                    "    local stock = tonumber(redis.call('get', KEYS[1]))\n" +
+                    "    if( stock <=0 ) then\n" +
+                    "        return -1\n" +
+                    "    end;\n" +
+                    "    redis.call('decr',KEYS[1]);\n" +
+                    "    return stock -1;\n" +
+                    "end;\n" +
+                    "return -1;";
+            Long stock = (Long) client.eval(script, Collections.singletonList(key), Collections.emptyList());
+
+            if (stock < 0) {
+                System.out.println("Stock not enough");
+                return false;
+            }
+            System.out.println("Congratulations! you have successully seckill the item.");
+            return true;
+        } catch (Throwable throwable) {
+            System.out.println("the error is:" + throwable.toString());
+            return false;
+        }
+    }
 }
+
+
+
+
+
