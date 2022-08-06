2e506f4501f7030d0f410cae1804c812ac32277a
ruoyuangao <89848323+ruoyuangao@users.noreply.github.com>
2022-08-05 20:34:30 -0500

    realize distributed lock using redis

--- seckill/src/test/java/com/jiuzhang/seckill/services/RedisDemoTest.java
+++ seckill/src/test/java/com/jiuzhang/seckill/services/RedisDemoTest.java
@@ -1,11 +1,10 @@
-package com.jiuzhang.seckill.mq;
+package com.jiuzhang.seckill.services;
 
-import com.jiuzhang.seckill.services.RedisService;
-import com.jiuzhang.seckill.services.SeckillActivityService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.UUID;
 
 @SpringBootTest
 public class RedisDemoTest {
@@ -61,6 +60,24 @@ public class RedisDemoTest {
         System.out.println(seclillInfo);
         String seckillCommodity = redisService.getValue("seckillCommodity:"+1001);
         System.out.println(seckillCommodity);
+    }
+
+    /** * 测试高并发下获取锁的结果 */
+    @Test public void testConcurrentAddLock() {
+        for (int i = 0; i < 10; i++) {
+            String requestId = UUID.randomUUID().toString();
+            // 打印结果 true false false false false false false false false false
+            // 只有第一个能获得 锁
+            System.out.println(redisService.tryGetDistributedLock("A", requestId,1000)); }
+    }
+
+    /** * 测试并发下获取锁然后立刻释放锁的结果 */
+    @Test public void testConcurrent() {
+        for (int i = 0; i < 10; i++) {
+            String requestId = UUID.randomUUID().toString();
+            // 打印结果 true true true true true true true true true true
+            System.out.println(redisService.tryGetDistributedLock("A", requestId,1000)); redisService.releaseDistributedLock("A", requestId);
+        }
     }
 
 }
